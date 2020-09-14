8e063385810a6cbf9783ab6df63d0a4c52f98de0
Ryan <zelda0169@gmailcom>
2020-09-14 17:32:21 -0600

    Add project files.

---
+++ Integers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    public class Integers
+    {
+        public int myMethod(int x)
+        {
+            int result = x / 2;
+            return result;
+        }
+        public int myMethod(decimal x)
+        {
+            decimal result = x * 2;
+            int final = Convert.ToInt32(result);
+            return final;
+        }
+        public int myMethod(string x)
+        {
+
+            int result = Convert.ToInt32(x);
+            result = result + 3;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number to divide");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Integers intobj1 = new Integers();
+            int result1 = intobj1.myMethod(num1);
+            Console.WriteLine(result1);
+
+            Console.WriteLine("Please enter a decimal");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+            int result2 = intobj1.myMethod(num2);
+            Console.WriteLine(result2);
+
+            Console.WriteLine("please type a number");
+            string num3 = (Console.ReadLine());
+            int result3 = intobj1.myMethod(num3);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
